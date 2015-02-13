f5a47f936a80dc0902054d2de2ddfdd07bf25459
Corentin Mors <mikescops@pixelswap.fr>
2015-02-13 16:42:15 +0100

    Clean some code

--- Extension.php
+++ Extension.php
@@ -22,27 +22,25 @@ class Extension extends \Bolt\BaseExtension
     public function disqus($title="")
     {
         $html = <<< EOM
+            <div id="livefyre-comments"></div>
+            <script type="text/javascript" src="http://zor.livefyre.com/wjs/v3.0/javascripts/livefyre.js"></script>
+            <script type="text/javascript">
+            (function () {
+                var articleId = fyre.conv.load.makeArticleId(null);
+                fyre.conv.load({}, [{
+                    el: 'livefyre-comments',
+                    network: "livefyre.com",
+                    siteId: "'%site_id%'",
+                    articleId: '%url%',
+                    signed: false,
+                    collectionMeta: {
+                        articleId: '%url%',
+                        url: fyre.conv.load.makeCollectionUrl(),
+                    }
+                }], function() {});
+            }());
+            </script>
 
-<!-- START: Livefyre Embed -->
-<div id="livefyre-comments"></div>
-<script type="text/javascript" src="http://zor.livefyre.com/wjs/v3.0/javascripts/livefyre.js"></script>
-<script type="text/javascript">
-(function () {
-    var articleId = fyre.conv.load.makeArticleId(null);
-    fyre.conv.load({}, [{
-        el: 'livefyre-comments',
-        network: "livefyre.com",
-        siteId: "'%site_id%'",
-        articleId: '%url%',
-        signed: false,
-        collectionMeta: {
-            articleId: '%url%',
-            url: fyre.conv.load.makeCollectionUrl(),
-        }
-    }], function() {});
-}());
-</script>
-<!-- END: Livefyre Embed -->
 
 EOM;
 
